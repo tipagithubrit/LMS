f9875b966007b84a4c56c3af238d253e824749f7
Karan singh mehra <mehrakannu2001@gmail.com>
2024-06-05 04:41:59 +0530

    Added checkout fail and payment cancellation page

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import Profile from './Pages/User/Profile';
 import EditPRofile from './Pages/User/EditProfile';
 import Checkout from './Pages/Payment/Checkout';
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
+import CheckoutFailure from './Pages/Payment/CheckoutFailure';
+// import CheckoutSuccess from './Pages/Payment/CheckoutSuccess.js';
 
 function App() {
   
@@ -46,6 +48,7 @@ function App() {
       <Route path='/user/editprofile' element={<EditPRofile />} />
       <Route path='/checkout' element={<Checkout />} />
       <Route path='/checkout/success' element={<CheckoutSuccess />} />
+      <Route path='/checkout/fail' element={<CheckoutFailure />} />
       </Route>
  
    <Route path="*" element={<NotFound />}></Route>
--- src/Pages/Payment/CheckoutFailure.jsx
+++ src/Pages/Payment/CheckoutFailure.jsx
@@ -1,32 +1,32 @@
-import { AiFillCheckCircle } from "react-icons/ai";
+import { RxCrossCircled } from "react-icons/rx";
 import HomeLayout from "../../Layouts/HomeLayout";
 import { Link } from "react-router-dom";
 
-function CheckoutSuccess() {
+function CheckoutFailure() {
 
     return (
         <HomeLayout>
             <div className="min-h-[90vh] flex items-center justify-center text-white">
                <div className="w-80 h-[26rem] flex flex-col justify-center items-center shadow-[0_0_10px_black] rounded-lg relative">
-                <h1 className="bg-green-500 absolute top-0 text-center w-full py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg"> 
-                    Payment successfull
+                <h1 className="bg-red-500 absolute top-0 text-center w-full py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg"> 
+                    Payment Failed
                 </h1>
 
                 <div className="px-4 flex felx-col items-center justify-center space-y-2">
                     <div className="text-center space-y-2">
                         <h2 className="text-lg font-semibold">
-                            Welcome to the pro bundle
+                            Oops ! Your payment failed
                         </h2>
                         <p className="text-left">
-                            Now you can enjoy all the courses
+                           Please try Again Later
 
                         </p>
-                        <AiFillCheckCircle className="text-green-500 text-5xl"/>
+                    <RxCrossCircled className="text-red-500 text-5xl"/>
 
                     </div>
-                    <Link to="/" className="bg-green-500 hover:bg-green-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full py-2 text-xl font-semibold text-center rounded-br-lg rounded-bl-lg">
+                    <Link to="/checkout" className="bg-red-500 hover:bg-green-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full py-2 text-xl font-semibold text-center rounded-br-lg rounded-bl-lg">
                     <button>
-                        Go to dashboard
+                        Try Again
                     </button>
                     </Link>
 
@@ -41,4 +41,4 @@ function CheckoutSuccess() {
 }
 
 
-export default CheckoutSuccess;
+export default CheckoutFailure;
